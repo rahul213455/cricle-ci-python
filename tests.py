f8da68ebf034df6ad9b7e23804fca01e1f13b151
rahul213455 singh <mrperfectritesh143@gmail.com>
2023-09-28 06:54:02 +0000

    pass test

--- tests.py
+++ tests.py
@@ -6,7 +6,7 @@ class MyTestCase(unittest.TestCase):
     def test_to_upper(self):
         name = "Ritesh"
         upper_name = to_upper(name)
-        self.assertEqual(upper_name, "RITESH dev1")
+        self.assertEqual(upper_name, "RITESH")
 
 
 if __name__ == '__main__':
